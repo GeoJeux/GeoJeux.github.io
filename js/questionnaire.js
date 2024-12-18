e6edef6d5c396da6bcbb667ca01b3d3fceca1e4b
MRC-code <cucmax19@ecolecatholique.ca>
2024-12-18 10:15:28 -0500

    Update questionnaire.js

--- js/questionnaire.js
+++ js/questionnaire.js
@@ -1,3 +1,4 @@
+//Tableau qui contient tous les pays du monde et leurs capitales 
 var tblPays = [
   ["AFGHANISTAN", "KABOUL"], ["AFRIQUE DU SUD", "PRETORIA"], ["ALBANIE", "TIRANA"], 
   ["ALGÉRIE", "ALGER"], ["ALLEMAGNE", "BERLIN"], ["ANDORRE", "ANDORRE-LA-VIEILLE"], 
@@ -67,83 +68,111 @@ var tblPays = [
   ["VIETNAM", "HANOÏ"], ["YÉMEN", "Sanaa"], ["ZAMBIE", "LUSAKA"], ["ZIMBABWE", "HARARE"]
 ];
 
-var vies = 3
-var pays;
-var score = 0;
+var vies = 3 // Initialise le nombre de vies à 3
+var pays; // Déclare une variable pour stocker le pays sélectionné
+var score = 0; // Initialise le score à 0
+
 
 function genere() {
-pays = Math.floor(Math.random() * tblPays.length);
-   var question = "Quelle est la capitale de " + tblPays[pays][0] + " ?";
+    // Génère un index aléatoire pour sélectionner un pays dans le tableau
+    pays = Math.floor(Math.random() * tblPays.length);
+    // Crée une question sur la capitale du pays sélectionné
+    var question = "Quelle est la capitale de " + tblPays[pays][0] + " ?";
 
-  document.getElementById("divAffiche").innerHTML = question
+    // Affiche la question dans l'élément HTML avec l'ID 'divAffiche'
+    document.getElementById("divAffiche").innerHTML = question;
 }
-  function jouer(){
+
+function jouer() {
+    // Crée le message HTML pour le jeu, incluant les vies, le score et les boutons
     var message = "<div id='vies'></div><div id='score'></div>\
       <div id='stop'><input type='button' value='Arrêter' onclick='arrete()'></div>\
       <div class='carte'></div>\
-       <div id = 'divAffiche'></div>\
+      <div id='divAffiche'></div>\
       <div class='emptySpace'></div>\
       <div>\
         <input type='text' id='guess'>\
         <input type='button' value='Deviner' id='devine' onclick='devine()'>\
         <div id='erreurs'></div>\
       </div>\
-      <div class='emptySpace'></div>"
+      <div class='emptySpace'></div>";
 
-    var affiche = document.getElementById("jeux")
+    // Affiche le message dans l'élément HTML avec l'ID 'jeux'
+    var affiche = document.getElementById("jeux");
     affiche.innerHTML = message;
-    document.getElementById("guess").focus()
+    // Met le focus sur le champ de saisie pour deviner
+    document.getElementById("guess").focus();
 
+    // Affiche le score actuel dans l'élément HTML avec l'ID 'score'
     var affiche = document.getElementById("score");
     affiche.innerHTML = "Score : " + score;
 
+    // Appelle la fonction pour afficher les vies restantes
     lives();
+    // Génère une nouvelle question
     genere();
-    }
-  function devine(){
+}
+
+function devine() {
+    // Récupère la valeur saisie par l'utilisateur, en la mettant en majuscules et en supprimant les espaces
     var essaie = document.getElementById("guess").value.toUpperCase().trim(" ");
-    if (essaie == tblPays[pays][1]){
-      message = "Bravo, vous avez réussit à deviner la capitale de " + tblPays[pays][0] 
-      score += 500;
+    // Vérifie si la réponse est correcte
+    if (essaie == tblPays[pays][1]) {
+        // Message de succès si la réponse est correcte
+        message = "Bravo, vous avez réussit à deviner la capitale de " + tblPays[pays][0];
+        // Augmente le score de 500 points
+        score += 500;
 
-      genere()
+        // Génère une nouvelle question
+        genere();
     } else {
-      vies--
-      message = "<br>Non, ce n'est pas " + essaie + ". Réessaye<br>";
+        // Diminue le nombre de vies si la réponse est incorrecte
+        vies--;
+        // Message d'erreur si la réponse est incorrecte
+        message = "<br>Non, ce n'est pas " + essaie + ". Réessaye<br>";
     }
 
-
+    // Met à jour l'affichage des vies restantes
     lives();
+    // Met à jour l'affichage du score
     var affiche = document.getElementById("score");
     affiche.innerHTML = "Score : " + score;
 
-    document.getElementById("erreurs").innerHTML = message
-    var textBox = document.getElementById("guess")
+    // Affiche le message d'erreur
+    document.getElementById("erreurs").innerHTML = message;
+    // Réinitialise le champ de saisie
+    var textBox = document.getElementById("guess");
     textBox.value = "";
-
-  }
-
+}
 
 function lives() {
-  var vieAffiche = document.getElementById("vies");
-  var imageHTML = "<img class='coeur' src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/800px-Heart_coraz%C3%B3n.svg.png' style='width: " + (15 + vies * 2) + "px;'>"; // Smaller hearts, smaller size increments
-  var vieMessage = "";
+    // Récupère l'élément HTML pour afficher les vies
+    var vieAffiche = document.getElementById("vies");
+    // Crée le code HTML pour afficher une image de cœur, en fonction du nombre de vies
+    var imageHTML = "<img class='coeur' src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/800px-Heart_coraz%C3%B3n.svg.png' style='width: " + (15 + vies * 2) + "px;'>"; // Cœurs plus petits, incréments de taille plus petits
+    var vieMessage = "";
 
-  if (vies > 0) {
-    for (var i = 0; i < vies; i++) {
-      vieMessage += imageHTML; // Add heart image for each life
+    // Vérifie si le joueur a encore des vies
+    if (vies > 0) {
+        // Ajoute une image de cœur pour chaque vie restante
+        for (var i = 0; i < vies; i++) {
+            vieMessage += imageHTML; // Ajoute l'image du cœur pour chaque vie
+        }
+    } else {
+        // Pop up "Game Over" si le joueur n'a plus de vies
+        popUp = document.createElement("div");
+        popUp.id = 'gameOver';
+        popUp.innerHTML = "Game Over<br>Vous avez perdu<br>Votre score est de \n<h1>" + score + "</h1><input type='button' value='Rejouer' onclick='location.reload();'></a><a href='index.html'><input type='button' value='Quitter'<a>";
+        document.body.appendChild(popUp);
     }
-  } else {
-    // Add your "Game Over" logic here (if necessary)
-    popUp = document.createElement("div");
-    popUp.id = 'gameOver';
-    popUp.innerHTML = "Game Over<br>Vous avez perdu<br>Votre score est de \n<h1>" + score + "</h1><input type='button' value='Rejouer' onclick='location.reload();'></a><a href='index.html'><input type='button' value='Quitter'<a>";
-    document.body.appendChild(popUp);
-  }
 
-  vieAffiche.innerHTML = "Vies: " + vieMessage;
+    // Met à jour l'affichage des vies
+    vieAffiche.innerHTML = "Vies: " + vieMessage;
 }
-function arrete(){
-  vies = 0;
-  lives()
+
+function arrete() {
+    // Met le nombre de vies à zéro pour arrêter le jeu
+    vies = 0;
+    // Met à jour l'affichage des vies
+    lives();
 }
